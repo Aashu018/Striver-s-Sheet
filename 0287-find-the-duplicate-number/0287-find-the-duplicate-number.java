0ffaf7c59f1a852f4e0e23d6f0a894398e6201d3
Ashish Agarwal <99171912+Aashu018@users.noreply.github.com>
2023-08-04 13:12:13 +0530

    Time: 4 ms (88.14%), Space: 56.2 MB (88.80%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,15 +1,31 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int i =1;
-       while(nums[0]!= nums[nums[0]]){
-          swap(nums,nums[0],0); 
-       }
-        return nums[0];
-    }
+//         int i =1;
+//        while(nums[0]!= nums[nums[0]]){
+//           swap(nums,nums[0],0); 
+//        }
+//         return nums[0];
+//     }
     
-    public void swap(int[] arr,int x, int y){
-        int temp = arr[x];
-        arr[x] = arr[y];
-        arr[y]  = temp;
-    }
+//     public void swap(int[] arr,int x, int y){
+//         int temp = arr[x];
+//         arr[x] = arr[y];
+//         arr[y]  = temp;
+//     }
+        // Slow and fast pointer approach
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+        
+        fast = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+}
 }
