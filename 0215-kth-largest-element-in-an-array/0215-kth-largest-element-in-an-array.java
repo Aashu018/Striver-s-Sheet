08b295bf92a0daba5d20b2969225a2fc46f1664f
Ashish Agarwal <99171912+Aashu018@users.noreply.github.com>
2023-08-20 01:01:50 +0530

    Time: 27 ms (60.51%), Space: 55.9 MB (31.99%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,18 +1,45 @@
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        
-        for(int i=0;i<nums.length;i++){
-            pq.add(nums[i]);
+   
+    public int partition(int[] arr, int left, int right) {
+    int pivot = arr[left] ;
+    int l = left + 1 ;
+    int r = right;
+    while (l <= r) {
+        if (arr[l] < pivot && arr[r] > pivot) {
+            int temp=arr[l];
+            arr[l]=arr[r];
+            arr[r]=temp;
+            l++ ;
+            r-- ;
         }
-        
-        int f = k-1;
-        while(f>0){
-            pq.remove();
-            f--;
+        if (arr[l] >= pivot) {
+            l++;
         }
-        int ans = pq.peek();
-        return ans;
+        if (arr[r] <= pivot) {
+            r--;
+        }
     }
+    int temp2=arr[left];
+            arr[left]=arr[r];
+            arr[r]=temp2;
+    return r;
+}
+
+ public int findKthLargest(int[] arr, int k) {
+    int left = 0, right = arr.length - 1, kth;
+    while (true) {
+        int idx = partition(arr, left, right);
+        if (idx == k - 1) {
+            kth = arr[idx];
+            break;
+        }
+        if (idx < k - 1) {
+            left = idx + 1;
+        } else {
+            right = idx - 1;
+        }
+    }
+    return kth;
+}
+
 }
