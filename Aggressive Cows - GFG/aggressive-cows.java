42ce398f9065c7a5ab538b1a4681024ab314d4d6
Ashish Agarwal <99171912+Aashu018@users.noreply.github.com>
2023-08-24 20:06:19 +0530

    Added solution - LeetHub

---
+++ Aggressive Cows - GFG/aggressive-cows.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t;
+        t = sc.nextInt();
+        while (t-- > 0) {
+
+            int n;
+            n = sc.nextInt();
+
+            int k;
+            k = sc.nextInt();
+
+            int[] v = new int[n];
+            for (int i = 0; i < n; i++) v[i] = sc.nextInt();
+
+            Solution obj = new Solution();
+            int res = obj.solve(n, k, v);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public static int solve(int n, int k, int[] stalls) {
+        Arrays.sort(stalls);
+        int lo = 0,hi = stalls[n-1]-stalls[0];
+        
+        while(lo<=hi){
+            int mid = (lo+hi)>>1;
+            
+            if(CanWePlace(stalls,k,mid)==true){
+                lo = mid+1;
+            }else{
+                hi = mid-1;
+            }
+        }
+        return hi;
+    }
+    public static boolean CanWePlace(int[] stalls,int cows, int dist){
+        int cntcows = 1;
+        int last = stalls[0];
+        for(int i=1;i<stalls.length;i++){
+            if(stalls[i]-last >= dist){
+                cntcows++;
+                last = stalls[i];
+            }
+            if(cntcows>=cows) return true;
+        }
+        return false;
+    }
+}
