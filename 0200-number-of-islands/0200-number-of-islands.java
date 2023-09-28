4a27ff370ff94632f0941af0c52fec4d5842fdda
Ashish Agarwal <99171912+Aashu018@users.noreply.github.com>
2023-09-28 20:32:25 +0530

    Time: 4 ms (43.15%), Space: 49.3 MB (26.25%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,42 +1,43 @@
-class Pair{
-    int first;
-    int second;
+// class Pair{
+//     int first;
+//     int second;
     
-    Pair(int f , int s){
-        first = f;
-        second = s;
-    }
-}
+//     Pair(int f , int s){
+//         first = f;
+//         second = s;
+//     }
+// }
 class Solution {
     private void bfs(int ro, int co, int[][] vis, char[][] grid) {
       vis[ro][co] = 1; 
-      Queue<Pair> q = new LinkedList<Pair>();
-      q.add(new Pair(ro, co)); 
+      // Queue<Pair> q = new LinkedList<Pair>();
+      // q.add(new Pair(ro, co)); 
       int n = grid.length; 
       int m = grid[0].length; 
       
       // until the queue becomes empty
-      while(!q.isEmpty()) {
-          int row = q.peek().first; 
-          int col = q.peek().second; 
-          q.remove(); 
+      // while(!q.isEmpty()) {
+      //     int row = q.peek().first; 
+      //     int col = q.peek().second; 
+      //     q.remove(); 
           
           // traverse in the neighbours and mark them if its a land 
           int[] delrow = {-1,0,1,0};
           int[] delcol = {0,1,0,-1};
          
               for(int i = 0;i<4; i++) {
-                  int nrow = row + delrow[i]; 
-                  int ncol = col + delcol[i]; 
+                  int nrow = ro + delrow[i]; 
+                  int ncol = co + delcol[i]; 
           // check if neighbour row and column is valid, and is an unvisited land
                   if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m 
                   && grid[nrow][ncol] == '1' && vis[nrow][ncol] == 0) {
-                      vis[nrow][ncol] = 1; 
-                      q.add(new Pair(nrow, ncol)); 
+                      // vis[nrow][ncol] = 1; 
+                      // q.add(new Pair(nrow, ncol)); 
+                      bfs(nrow,ncol,vis,grid);
                   }
               }
           
-      }
+      //}
   }
 
     // Function to find the number of islands.
