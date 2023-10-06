cd095705ae1f0d2bc592dbcf5299ea367d568d5a
Ashish Agarwal <99171912+Aashu018@users.noreply.github.com>
2023-10-06 15:06:22 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (30.10%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,22 +15,38 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-          HashMap<Node,Node> hashMap=new HashMap<>();
-    Node temp = head;
-//first iteration for inserting deep nodes of every node in the hashmap.
+        Node temp = head; 
+// Step 1
     while(temp != null) {
-        Node newNode = new Node(temp.val);
-        hashMap.put(temp,newNode);
-        temp = temp.next;
+        Node newnode = new Node(temp.val);
+        newnode.next = temp.next;
+        temp.next = newnode;
+        temp = temp.next.next;
     }
-    Node t = head;
-//second iteration for linking next and random pointer as given question.
-    while(t != null) {
-        Node node = hashMap.get(t);
-        node.next = (t.next != null) ? hashMap.get(t.next):null;
-        node.random = (t.random != null) ? hashMap.get(t.random):null;
-        t = t.next;
+// Step 2
+    Node itr = head;
+
+    while(itr != null) {
+        if(itr.random != null)
+       itr.next.random = itr.random.next;
+        
+        itr = itr.next.next;
     }
-    return hashMap.get(head);
+        
+// Step 3
+        
+     Node dummy = new Node(0);
+     temp = dummy;
+     itr = head;
+     Node fast ;  
+        while(itr != null){
+           fast = itr.next.next;
+            temp.next = itr.next;
+            itr.next = fast;
+            temp = temp.next;
+            itr = fast;
+        }
+        
+       return dummy.next; 
     }
 }
