6320499d5cbbe363647d8040b8f8017757be24f8
Ashish Agarwal <99171912+Aashu018@users.noreply.github.com>
2023-10-08 20:02:07 +0530

    Time: 1 ms (94.66%), Space: 43.5 MB (89.30%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,26 +1,30 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> ds = new ArrayList<>();
-        boolean[] freq = new boolean[nums.length];
-        find(nums,ans,ds,freq);
+        find(0,nums,ans);
         return ans;
     }
     
-    private void find(int nums[], List<List<Integer>> ans, List<Integer> ds, boolean freq[]){
-        if(ds.size() == nums.length){
+    private void find(int idx,int nums[], List<List<Integer>> ans){
+        if(idx == nums.length){
+            List<Integer> ds = new ArrayList<>();
+            for(int i=0;i<nums.length;i++){
+                ds.add(nums[i]);
+            }
             ans.add(new ArrayList<>(ds));
             return;
         }
         
-        for(int i=0;i<nums.length;i++){
-            if(!freq[i]){
-                freq[i]=true;
-                ds.add(nums[i]);
-                find(nums,ans,ds,freq);
-                ds.remove(ds.size()-1);
-                freq[i]=false;
-            }
+        for(int i=idx;i<nums.length;i++){
+            swap(i,idx,nums);
+            find(idx+1,nums,ans);
+            swap(i,idx,nums);
         }
+    }
+    
+    private void swap(int i, int j , int arr[]){
+        int t = arr[i];
+        arr[i]=arr[j];
+        arr[j]=t;
     }
 }
