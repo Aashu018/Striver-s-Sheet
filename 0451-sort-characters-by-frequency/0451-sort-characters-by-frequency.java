a1c5f1d617b733f6fff3447f017ba8d4487daa98
Ashish Agarwal <99171912+Aashu018@users.noreply.github.com>
2023-10-12 23:28:27 +0530

    Time: 4 ms (95.13%), Space: 43.4 MB (96.45%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,23 +1,43 @@
 class Solution {
+    class Pair{
+        char ch;
+        int count;
+        
+        Pair(char ch,int count){
+            this.ch = ch;
+            this.count = count;
+        }
+    }
     public String frequencySort(String s) {
-         HashMap<Character, Integer> map=new HashMap<>();
-        for(int i=0; i<s.length(); i++){
-            if(map.containsKey(s.charAt(i))){
-                map.put(s.charAt(i),map.get(s.charAt(i))+1);
-            }else{
-                map.put(s.charAt(i),1);
-            }            
+        int[] arr = new int[125];
+        
+        for(char ch : s.toCharArray()){
+            arr[ch]++;
         }
         
-        String ans="";
-        PriorityQueue<Character> pq=new PriorityQueue<>((a,b)->(map.get(b)-map.get(a)));
-        pq.addAll(map.keySet());
-        while(!pq.isEmpty()){
-           char ch=pq.poll();
-           for(int i=0; i<map.get(ch); i++){
-               ans+=ch+"";
-           }
+        LinkedList<Pair> freq_list = new LinkedList<>();
+        
+        for(int i=48;i<=122;i++){
+            if(arr[i]>0){
+                freq_list.add(new Pair((char)i,arr[i]));
+            }
         }
-        return ans;
+        
+        Collections.sort(freq_list,new Comparator<Pair>(){
+            public int compare(Pair a, Pair b){
+                return b.count - a.count;
+            }
+        });
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(Pair p: freq_list){
+            int count = p.count;
+            while(count-->0){
+                sb.append(p.ch);
+            }
+        }
+        
+        return sb.toString();
     }
 }
