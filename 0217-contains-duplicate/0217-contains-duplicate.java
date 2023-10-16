245ce0aa729639e7f578b3b4cd38546de01ed891
Ashish Agarwal <99171912+Aashu018@users.noreply.github.com>
2023-10-17 03:03:31 +0530

    Time: 10 ms (84.62%), Space: 54.6 MB (86.36%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> hm = new HashSet<>();
+        
+        for(int i=0;i<nums.length;i++){
+            if(hm.contains(nums[i])){
+                return true;
+            }
+            hm.add(nums[i]);
+        }
+        return false;
+    }
+}
