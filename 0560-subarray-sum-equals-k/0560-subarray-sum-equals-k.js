2d5cedf52dd62ba094baa76040e0239e52ac5930
Ashish Agarwal <99171912+Aashu018@users.noreply.github.com>
2023-12-05 15:26:13 +0530

    Time: 90 ms (37.67%), Space: 54.2 MB (14.20%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+let subarraySum = (nums, k) => {
+  let map = { 0: 1 };
+  let count = 0;
+  let sum = 0;
+  for (let idx = 0, len = nums.length; idx < len; idx++) {
+    sum += nums[idx];
+    if (map[sum - k]) {
+      count += map[sum - k];
+    }
+    map[sum] = (map[sum] || 0) + 1;
+  }
+  return count;
+};
