4a6b3d802d30c601700b38b388aa003f2df1d05d
Ashish Agarwal <99171912+Aashu018@users.noreply.github.com>
2023-12-07 18:14:52 +0530

    Time: 58 ms (51.48%), Space: 41.7 MB (64.13%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -3,10 +3,25 @@
  * @return {number}
  */
 var fib = function(n) {
-    if (n <= 1) {
-    return n;
-  } else {
-    return fib(n - 1) + fib(n - 2);
-  };
+     var memo = {};
 
+  // Define the recursive function
+  function fib(n) {
+    // Check if the value is already stored in the memo
+    if (n in memo) {
+      return memo[n];
+    }
+
+    // Base cases
+    if (n <= 1) {
+      return n;
+    }
+
+    // Calculate and store the value in the memo
+    memo[n] = fib(n - 1) + fib(n - 2);
+    return memo[n];
+  }
+
+  // Call the recursive function with the desired value
+  return fib(n);
 };
