95cec8998c3b2b034cf6619acce2e2a556f8eda9
Ashish Agarwal <99171912+Aashu018@users.noreply.github.com>
2023-12-07 18:12:40 +0530

    Time: 82 ms (10.81%), Space: 41.8 MB (52.59%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -3,12 +3,10 @@
  * @return {number}
  */
 var fib = function(n) {
-    let a = 0, b = 1;
-  for (let i = 0; i < n; i++) {
-    let temp = a;
-    a = b;
-    b = temp + a;
-  }
-  return a;
+    if (n <= 1) {
+    return n;
+  } else {
+    return fib(n - 1) + fib(n - 2);
+  };
 
 };
