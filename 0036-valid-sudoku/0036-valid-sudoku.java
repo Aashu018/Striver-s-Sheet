5a08edb0412d890e085421f4317d548a616d37f3
Ashish Agarwal <99171912+Aashu018@users.noreply.github.com>
2024-03-23 11:09:39 +0530

    Time: 13 ms (30.69%), Space: 44.4 MB (53.13%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,19 +1,19 @@
 class Solution {
-     public boolean isValidSudoku(char[][] board) {
-        //rows
-        for(int i=0;i<=8;i++) {
-            Set<Character> set=new HashSet<>();
-            for(int j=0;j<=8;j++) if(board[i][j]!='.') if(!set.add(board[i][j])) return false;
-        }
-        //column
-        for(int j=0;j<9;j++) {
-            Set<Character> set=new HashSet<>();
-            for(int i=0;i<9;i++) if(board[i][j]!='.') if(!set.add(board[i][j])) return false;
-        }
-        //box
-        for(int x=0;x<=6;x+=3) for(int y=0;y<=6;y+=3) {
-            Set<Character> set=new HashSet<>();
-            for(int i=x;i<=x+2;i++) for(int j=y;j<=y+2;j++) if(board[i][j]!='.') if(!set.add(board[i][j])) return false;
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> set = new HashSet<>();
+        
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                
+                char ch = board[i][j];
+                if(ch != '.'){
+                    if(!set.add(ch + "found in row" + i) ||
+                       ! set.add(ch + "found in col" + j) ||
+                       ! set.add(ch + "found in matrix" + i/3 + j/3)){
+                        return false;
+                    }
+                }
+            }
         }
         return true;
     }
